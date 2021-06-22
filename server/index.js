0643997fea0691f8f458764f7a3b485a02ac9caa
Kyle Scudder <kyle.s@paloma.co.uk>
2021-06-22 17:43:06 +0100

    Added path to server .env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config({path:__dirname+'/.env'})
 const cookieParser = require('cookie-parser')
 
 const db = require('./db')
